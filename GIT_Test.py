130182c6a00a7ff52d9e5c1267afe4907dd720d9
Oskari Lehto <oskari.lehto@edu.turkuamk.fi>
2022-09-22 09:22:58 +0300

    Added new print message to GIT_Test.py

--- GIT_Test.py
+++ GIT_Test.py
@@ -2,4 +2,4 @@ Python 3.9.13 (tags/v3.9.13:6de2ca5, May 17 2022, 16:36:42) [MSC v.1929 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> print("I am a beginner with Git")
 I am a beginner with Git
->>> 
+>>> print ("I am learning Git")
